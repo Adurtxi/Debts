9d8f4f6c5bbea38dbefc44856a76a367b339e134
Adur Marques <adur.marques@gmail.com>
2020-03-29 14:34:57 +0200

    Followed

--- lib/src/widgets/followers/followerList.dart
+++ lib/src/widgets/followers/followerList.dart
@@ -1,3 +1,5 @@
+import 'package:epbasic_debts/src/blocs/followers_bloc.dart';
+import 'package:epbasic_debts/src/blocs/provider.dart';
 import 'package:epbasic_debts/src/models/follower_model.dart';
 import 'package:epbasic_debts/src/preferences/user_preferences.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +15,8 @@ class FollowerList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _followersBloc = Provider.followersBloc(context);
+
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
@@ -37,11 +41,40 @@ class FollowerList extends StatelessWidget {
               ),
               title: Text(
                   '${follower.follower.name} ${follower.follower.surname}'),
-              trailing: Icon(Icons.person),
+              trailing: _trailing(_followersBloc),
             ),
           ),
         ],
       ),
     );
+  }
+
+  Widget _trailing(FollowersBloc _followersBloc) {
+    if (follower.accepted == true) {
+      return FlatButton(
+        child: Text('Eliminar'),
+        onPressed: () {
+          _followersBloc.deleteFollower(follower.followerId);
+        },
+      );
+    } else {
+      return ButtonBar(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          new FlatButton(
+            child: new Text('Cancelar'),
+            onPressed: () {
+              _followersBloc.deleteFollower(follower.followerId);
+            },
+          ),
+          new FlatButton(
+            child: new Text('Aceptar'),
+            onPressed: () {
+              _followersBloc.acceptFollower(follower.followerId);
+            },
+          ),
+        ],
+      );
+    }
   }
 }
